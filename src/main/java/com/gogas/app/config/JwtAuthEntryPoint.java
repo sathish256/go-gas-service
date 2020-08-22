e86a6f23b7aba8d3c0d5f03e5d95307d3afdf9ab
Sathish Krishnamurthy <satheez.krish@gmail.com>
2020-08-22 22:57:59 +0530

    Test Autorization

--- src/main/java/com/gogas/app/config/JwtAuthEntryPoint.java
+++ src/main/java/com/gogas/app/config/JwtAuthEntryPoint.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.gogas.app.model.dto.GoGasResponse;
 
 @Component
@@ -24,13 +25,14 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint, Serializable
 	 * 
 	 */
 	private static final long serialVersionUID = -4004839843606501271L;
-
+	
 	@Autowired
 	ObjectMapper objectMapper;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
+		
 		GoGasResponse apiError = new GoGasResponse(HttpStatus.UNAUTHORIZED);
 		apiError.setMessage("Authentication Failure");
 		String json = objectMapper.writeValueAsString(apiError);
