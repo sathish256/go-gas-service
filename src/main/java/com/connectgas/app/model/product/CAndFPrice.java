d42d164540c65ac03affbc55793fd8b13270448a
Sathish Krishnamurthy <satheez.krish@gmail.com>
2020-09-01 11:42:27 +0530

    DealerPrice and CAndFprice update

--- src/main/java/com/connectgas/app/model/product/CAndFPrice.java
+++ src/main/java/com/connectgas/app/model/product/CAndFPrice.java
@@ -4,16 +4,25 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "candfPrice")
 public class CAndFPrice {
 
 	@Id
-	@Column(name = "candfId", unique = true, nullable = false)
+	@GeneratedValue(generator = "cndp-uuid")
+	@GenericGenerator(name = "cndp-uuid", strategy = "uuid2")
+	@Column(name = "id", unique = true)
+	private String id;
+
+	@Column(name = "candfId")
 	private String candfId;
+
 	@Column(name = "price")
 	private BigDecimal price;
 
@@ -31,6 +40,14 @@ public class CAndFPrice {
 
 	public void setCandfId(String candfId) {
 		this.candfId = candfId;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
--- src/main/java/com/connectgas/app/model/product/DealerPrice.java
+++ src/main/java/com/connectgas/app/model/product/DealerPrice.java
@@ -4,14 +4,22 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "dealerprice")
 public class DealerPrice {
 
 	@Id
+	@GeneratedValue(generator = "dsp-uuid")
+	@GenericGenerator(name = "dsp-uuid", strategy = "uuid2")
+	@Column(name = "id", unique = true)
+	private String id;
+
 	@Column(name = "dealerId", nullable = false)
 	private String dealerId;
 
@@ -32,6 +40,14 @@ public class DealerPrice {
 
 	public void setPrice(BigDecimal price) {
 		this.price = price;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
