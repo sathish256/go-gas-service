e87d79f5eef807bd3f665c8468881f2dbb5e29a6
Sathish Krishnamurthy <satheez.krish@gmail.com>
2020-09-06 20:33:37 +0530

    Added WS Authorization

--- src/main/java/com/connectgas/app/notification/WebSocketConfig.java
+++ src/main/java/com/connectgas/app/notification/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	 */
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/queue");
+		config.enableSimpleBroker("/queue","/user");
 		config.setPreservePublishOrder(true);
 	}
 
